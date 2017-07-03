7254d73fdc61d88496b62e4642eb3d0004415cee
anil <r008611b@student.staffs.ac.uk>
2017-07-03 17:10:31 +0100

    Commit All Files - Initial

---
+++ Rename File Names/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Rename_File_Names
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetFiles
+            // 
+            this.btnGetFiles.Location = new System.Drawing.Point(103, 114);
+            this.btnGetFiles.Name = "btnGetFiles";
+            this.btnGetFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnGetFiles.TabIndex = 0;
+            this.btnGetFiles.Text = "Get Files";
+            this.btnGetFiles.UseVisualStyleBackColor = true;
+            this.btnGetFiles.Click += new System.EventHandler(this.btnGetFiles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGetFiles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetFiles;
+    }
+}
+
---
+++ Rename File Names/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Web;
+
+
+namespace Rename_File_Names
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetFiles_Click(object sender, EventArgs e)
+        {
+            var browser = new FolderBrowserDialog();
+            var path = "";
+
+            if (browser.ShowDialog() == DialogResult.OK)
+            {
+                path = browser.SelectedPath; // prints path
+                Console.WriteLine(path);
+            }
+
+            //lets you create/move and enumerate files from a path
+            var directoryInfo = new DirectoryInfo(path);
+            //gets list of the actual files in that folder
+            var fileInfo = directoryInfo.GetFiles();
+
+            foreach (var file in fileInfo)
+            {
+                var fileSplit = file.FullName.Split('.');
+                var newFileName = fileSplit[0] + " " + fileSplit[1] + " (" + fileSplit[2] + ")" + file.Extension;
+                
+                File.Move(file.FullName,newFileName);
+            }
+        }
+    }
+}
